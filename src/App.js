4ffdc1b0396a47a24580bda87da6382bfb2a40e6
KHUNBIRD <birdpakornpong@gmail.com>
2022-05-03 02:44:16 +0700

    hidden nav when scroll to section 2 in page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
 import NavComponent from './components/NavBarComponent'
 import ContentComponent from './components/ContentComponent';
 import CarouselComponent from './components/CarouselComponent'
@@ -8,9 +9,24 @@ import FooterComponent from './components/FooterComponent'
 import ImgComponent from './components/ImgComponent';
 
 function App() {
+  const [scrollPosition, setPosition] = useState(0);
+
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setPosition(position);
+    };
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+    
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
   return (
     <div className="App">
-      <NavComponent />
+      {scrollPosition < 700 ? <NavComponent /> : ""}
+      {/* <NavComponent /> */}
       <ImgComponent />
       <ContentImgComponent />
       <CarouselComponent />
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Form, FormControl, Button, Nav } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import "./FooterComponent.css"
 
 export default function FooterComponent() {
   return (
-    <div className="ifooter">
-        <h1>footer</h1>
+    <div className="footer-i">
+        <Row className='h-100 clear-row-i'>
+          {/* <Col xs="12" md="4" className='footer-owner-i'>
+            <h5>Bird Pakornpong</h5>
+            <h6>+66 (0)64 5420333</h6>
+            <h6>birdpakorn@gmail.com</h6>
+          </Col> */}
+          <Col xs="12" md="12">
+            <p>About | Content | Product | Promotion | Card</p>
+          </Col>
+          <Col xs="12" className='footer-end-i'>
+            <p>© 2022 Blue Canyon Country Club. All rights reserved.</p>
+          </Col>
+        </Row>
     </div>
   )
 }
