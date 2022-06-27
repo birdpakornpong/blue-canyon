d84c432ca3fe201d2e761eb146c5e86e0dd9525d
KHUNBIRD <birdpakornpong@gmail.com>
2022-06-28 05:03:37 +0700

    meterail UI

--- src/pages/Link.js
+++ src/pages/Link.js
@@ -5,6 +5,8 @@ import QueryComponent from "../components/QueryComponent";
 import QueryPostComponent from "../components/QueryPostComponent";
 import About from "../components/About";
 import NoteState from "../context/notes/NoteState";
+import InputPreviousValue from "../components/InputPreviousValue";
+import CheckBox from "../components/CheckBox";
 
 export default function Link() {
   return (
@@ -16,6 +18,8 @@ export default function Link() {
       <NoteState>
         <About />
       </NoteState>
+      <InputPreviousValue />
+      <CheckBox />
     </>
   );
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ImgComponent from "../components/ImgComponent";
-
+import ControlledAccordions from "../components/meterialUI/ControlledAccordions";
 export default function Product() {
   return (
     <>
       <ImgComponent />
+      <ControlledAccordions />
     </>
   );
 }
